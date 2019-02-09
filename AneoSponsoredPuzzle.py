cad25563f5b55aabe02d5b46d87687600952d279
Marcos Barrera <marcosbarrera@gmail.com>
2019-02-09 09:20:27 -0800

    Initial Commit of Aneo Sponsored Puzzle

---
+++ AneoSponsoredPuzzle.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+speed = int(input())
+light_count = int(input())
+for i in range(light_count):
+    distance, duration = [int(j) for j in input().split()]
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print("answer")
