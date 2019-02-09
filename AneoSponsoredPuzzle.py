02feb933d15c6dc00a2fa99154ab344aaae67241
Marcos Barrera <marcosbarrera@gmail.com>
2019-02-09 09:38:25 -0800

    Add print messages to see dynamic inputs

--- AneoSponsoredPuzzle.py
+++ AneoSponsoredPuzzle.py
@@ -8,8 +8,11 @@ speed = int(input())
 light_count = int(input())
 for i in range(light_count):
     distance, duration = [int(j) for j in input().split()]
+    print("distance %d: %d"% (i, distance), file=sys.stderr)
+    print("duration %d: %d"% (i, duration), file=sys.stderr)
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
-
+print(speed, file=sys.stderr)
+print(light_count, file=sys.stderr)
 print("answer")
